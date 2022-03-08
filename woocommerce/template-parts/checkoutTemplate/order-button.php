9547d7ec786540cd53b72e4b99779692afe51fd3
Rémi TROTEL <remi.trotel@epita.fr>
2022-03-08 08:23:58 +0100

    yfghjk

--- woocommerce/template-parts/checkoutTemplate/order-button.php
+++
@@ -1,3 +0,0 @@
-<?php echo '<div class="u-align-right u-form-group u-form-submit">
-                        <a onclick="submitCheckoutForm();" class="u-btn u-btn-submit u-button-style u-btn-1">' . __('Place order', 'woocommerce') . '</a>
-                    </div>'; ?>
